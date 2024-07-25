29c53389581ee8aa3ee514a51e6a3076e5f00599
ebogyeong <bogyeong.lee@growdle.com>
2024-07-25 15:40:36 +0900

    feature: design button component

--- src/shared/ui/Button/index.stories.tsx
+++ src/shared/ui/Button/index.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import Link from 'next/link'
+
 import { Button } from '.'
 
 const meta: Meta<typeof Button> = {
@@ -13,5 +15,34 @@ export default meta
 type Story = StoryObj<typeof Button>
 
 export const Default: Story = {
-  args: { children: 'btn' }
+  render: () => {
+    return <Button>button</Button>
+  }
+}
+
+export const Stretch: Story = {
+  render: () => {
+    return <Button stretch>button</Button>
+  }
+}
+
+export const LinkButton: Story = {
+  render: () => {
+    return <Button variant="link">login</Button>
+  }
+}
+
+export const NavLink: Story = {
+  render: () => {
+    return (
+      <>
+        <Button asChild>
+          <Link href="#">Get Started for Free</Link>
+        </Button>
+        <Button asChild variant="secondary">
+          <Link href="#">See Examples</Link>
+        </Button>
+      </>
+    )
+  }
 }
