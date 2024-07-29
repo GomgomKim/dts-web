45082219729781edf9c897b0d8e7dfb86e211e78
ebogyeong <bogyeong.lee@growdle.com>
2024-07-29 18:33:09 +0900

    feature: add ExportButton component

---
+++ src/features/archive/ui/ExportButton/ui.tsx
@@ -0,0 +1,31 @@
+import { useCallback } from 'react'
+import { toPng } from 'html-to-image'
+import { Button } from '@/sdcn/components/ui/Button'
+
+interface ExportButtonProps {
+  containerRef: React.RefObject<HTMLElement>
+}
+
+export const ExportButton = ({ containerRef }: ExportButtonProps) => {
+  const onButtonClick = useCallback(() => {
+    console.log(containerRef.current)
+    if (containerRef.current === null) return
+
+    toPng(containerRef.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement('a')
+        link.download = 'my-image-name.png'
+        link.href = dataUrl
+        link.click()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [containerRef])
+
+  return (
+    <Button variant="outline" onClick={onButtonClick}>
+      Download
+    </Button>
+  )
+}
