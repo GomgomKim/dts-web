add78e60cd89d97eda531ab491a8795f6bbd6c4c
ebogyeong <bogyeong.lee@growdle.com>
2024-07-30 20:44:57 +0900

    feature: implement RadioGroup component

---
+++ src/shared/ui/RadioGroup/ui.tsx
@@ -0,0 +1,52 @@
+import { cn } from '@/sdcn/lib/utils'
+import React from 'react'
+import { RadioGroupContext } from './index.context'
+
+type RadioGroupProps = React.ComponentProps<'div'> & {
+  value: string
+  onValueChange: (value: string) => void
+}
+const RadioGroup = React.forwardRef<HTMLDivElement, RadioGroupProps>(
+  ({ value, onValueChange, className, ...props }, ref) => {
+    const contextValue = { value, onValueChange }
+
+    return (
+      <RadioGroupContext.Provider value={contextValue}>
+        <div className={cn('flex gap-2', className)} {...props} ref={ref} />
+      </RadioGroupContext.Provider>
+    )
+  }
+)
+RadioGroup.displayName = 'RadioGroup'
+
+type RadioGroupItemProps = React.ComponentProps<'div'> & {
+  value: string
+  label: string
+}
+const RadioGroupItem = React.forwardRef<HTMLDivElement, RadioGroupItemProps>(
+  ({ value: localValue, label, className, ...props }, ref) => {
+    const { value, onValueChange } = React.useContext(RadioGroupContext)!
+
+    return (
+      <div
+        ref={ref}
+        role="radio"
+        aria-checked={value === localValue}
+        className={cn(
+          'p-3 rounded-[0.5rem] bg-inherit text-neutral-7 border border-neutral-1 cursor-pointer flex items-center',
+          className,
+          {
+            'bg-neutral-1 text-white': value === localValue
+          }
+        )}
+        onClick={() => onValueChange(localValue)}
+        {...props}
+      >
+        <label className="cursor-pointer">{label}</label>
+      </div>
+    )
+  }
+)
+RadioGroupItem.displayName = 'RadioGroupItem'
+
+export { RadioGroup, RadioGroupItem }
