6e6017fb32a19a84e3694e887e2da1677953807a
ebogyeong <bogyeong.lee@growdle.com>
2024-07-30 21:25:17 +0900

    feature: add radio options to detail page

--- src/views/model/ui/ui.tsx
+++ src/views/model/ui/ui.tsx
@@ -1,21 +1,44 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import './styles.css'
 import { ImageEditingBox } from '@/features/archive/ui/ImageEditingBox'
 import { ImageInputBox } from '@/features/archive/ui/ImageInputBox'
 import { Button } from '@/sdcn/components/ui/Button'
-import './styles.css'
+import { RadioGroup, RadioGroupItem } from '@/shared/ui/RadioGroup'
+
+const skinTextureOptions = ['Matte', 'Medium', 'Glowy']
+const aspectRatioOptions = ['16:9', '9:16', '1:1', '4:3', '3:4']
+const faceAngleOptions = ['Left', 'Front', 'Right']
 
 function Model({ modelName }: { modelName: string }) {
   console.log(modelName)
+
+  const [query, setQuery] = useState({
+    skinTexture: skinTextureOptions[0],
+    aspectRatio: aspectRatioOptions[0],
+    faceAngle: faceAngleOptions[0]
+  })
+
+  const handleValueChange = (id: string, value: string) => {
+    return setQuery((prev) => ({ ...prev, [`${id}`]: value }))
+  }
+
+  useEffect(() => {
+    console.log(query)
+  }, [query])
+
   return (
     <div className="gap-[40px] grid-areas-layout">
       {/* brand assets section*/}
       <section className="grid-areas-left flex flex-col gap-[20px]">
-        <h2 className="text-[24px]">Brand Assets</h2>
+        <h2 className="text-[24px] mb-5">Brand Assets</h2>
         <div>
-          <h3>Product</h3>
+          <h3 className="mb-3">Product</h3>
           <ImageInputBox />
         </div>
         <div>
-          <h3>Brand Logo</h3>
+          <h3 className="mb-3">Brand Logo</h3>
           <ImageInputBox />
         </div>
         <div className="flex flex-col">
@@ -31,8 +54,8 @@ function Model({ modelName }: { modelName: string }) {
           <div className="grid-areas-generate-editing">
             <ImageEditingBox />
           </div>
-          <div className="grid-area-generate-variations">
-            <h3>Variations</h3>
+          <div className="grid-area-generate-variations mt-[58px]">
+            <h3 className="mb-5">Variations</h3>
             <div
               style={{
                 width: '100%',
@@ -42,13 +65,40 @@ function Model({ modelName }: { modelName: string }) {
             ></div>
           </div>
           <div className="grid-area-generate-skin">
-            <h3>Skin Texture</h3>
+            <h3 className="mb-5">Skin Texture</h3>
+            <RadioGroup
+              id="skinTexture"
+              value={query.skinTexture}
+              onValueChange={handleValueChange}
+            >
+              {skinTextureOptions.map((option) => (
+                <RadioGroupItem key={option} value={option} label={option} />
+              ))}
+            </RadioGroup>
           </div>
           <div className="grid-area-generate-faceangle">
-            <h3>Aspect Ratio</h3>
+            <h3 className="mb-5">Aspect Ratio</h3>
+            <RadioGroup
+              id="aspectRatio"
+              value={query.aspectRatio}
+              onValueChange={handleValueChange}
+            >
+              {aspectRatioOptions.map((option) => (
+                <RadioGroupItem key={option} value={option} label={option} />
+              ))}
+            </RadioGroup>
           </div>
           <div className="grid-area-generate-aspectratio">
-            <h3>Face Angle</h3>
+            <h3 className="mb-5">Face Angle</h3>
+            <RadioGroup
+              id="faceAngle"
+              value={query.faceAngle}
+              onValueChange={handleValueChange}
+            >
+              {faceAngleOptions.map((option) => (
+                <RadioGroupItem key={option} value={option} label={option} />
+              ))}
+            </RadioGroup>
           </div>
         </div>
       </section>
