07a3dd1eb7e0d4029275ed33e34069c1991df42a
ebogyeong <bogyeong.lee@growdle.com>
2024-08-06 16:03:25 +0900

    refactor: separate options state management in Model component

--- src/views/model/ui/ui.tsx
+++ src/views/model/ui/ui.tsx
@@ -1,16 +1,12 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import Image from 'next/image'
 import './styles.css'
 import { ImageEditingBox } from '@/features/archive/ui/image-editing-box'
 import { ImageInputBox } from '@/features/archive/ui/image-input-box'
 import { Button } from '@/shared/ui/button'
 import { RadioGroup, RadioGroupItem } from '@/shared/ui/radio-group'
-
-import LeftIcon from '/public/icons/arrow-left.svg'
-import RightIcon from '/public/icons/arrow-right.svg'
-import Img1 from '/public/images/model-gen-1.png'
+import { VariationsSection } from '@/features/archive/ui/variations-section'
 
 const skinTextureOptions = ['Matte', 'Medium', 'Glowy']
 const aspectRatioOptions = ['16:9', '9:16', '1:1', '4:3', '3:4']
@@ -19,33 +15,43 @@ const faceAngleOptions = ['Left', 'Front', 'Right']
 function Model({ modelName }: { modelName: string }) {
   console.log(modelName)
 
-  const [query, setQuery] = useState({
-    skinTexture: skinTextureOptions[0],
-    aspectRatio: aspectRatioOptions[0],
-    faceAngle: faceAngleOptions[0]
-  })
+  // TODO:
+  // 받아온 데이터에 options로 라디오 버튼 값 넣어주기
+  // option 변경시 해당 모델에 그 옵션이 적용된 이미지 보여주기
 
-  const handleValueChange = (id: string, value: string) => {
-    return setQuery((prev) => ({ ...prev, [`${id}`]: value }))
+  const [skinTexture, setSkinTexture] = useState(skinTextureOptions[0])
+  const [aspectRatio, setAspectRatio] = useState(aspectRatioOptions[0])
+  const [faceAngle, setFaceAngle] = useState(faceAngleOptions[0])
+
+  const handleSkinTextureChange = (value: string) => {
+    setSkinTexture(value)
+  }
+
+  const handleAspectRatioChange = (value: string) => {
+    setAspectRatio(value)
+  }
+
+  const handleFaceAngleChange = (value: string) => {
+    setFaceAngle(value)
   }
 
   useEffect(() => {
-    console.log(query)
-  }, [query])
+    console.log(skinTexture, aspectRatio, faceAngle)
+  }, [skinTexture, aspectRatio, faceAngle])
 
   return (
     <div className="flex">
       {/* brand assets section*/}
-      <div className="flex-shrink-0 basis-[390px] mr-5">
+      <div className="flex-shrink-0 basis-[387px] mr-5">
         <section className="sticky top-0 flex flex-col gap-5">
           <h2 className="text-[24px] mb-5">Brand Assets</h2>
           <div>
             <h3 className="mb-3">Product</h3>
-            <ImageInputBox />
+            <ImageInputBox boxId="product" />
           </div>
           <div>
             <h3 className="mb-3">Brand Logo</h3>
-            <ImageInputBox />
+            <ImageInputBox boxId="logo" />
           </div>
           <div className="flex flex-col">
             <Button variant="outline">Remove Background</Button>
@@ -56,7 +62,7 @@ function Model({ modelName }: { modelName: string }) {
 
       {/* generate section */}
       <section className="px-5 grow">
-        <h2 className="text-[24px] -mb-[42px]">Generate</h2>
+        <h2 className="text-[24px] -mb-[37px]">Generate</h2>
         <div className="grid-areas-generate-layout gap-[40px]">
           {/* image editing section */}
           <div className="grid-areas-generate-editing">
@@ -65,48 +71,7 @@ function Model({ modelName }: { modelName: string }) {
 
           {/* variations */}
           <div className="grid-area-generate-variations mt-[58px]">
-            <div className="flex justify-between items-center mb-5">
-              <h3>Variations</h3>
-              <div className="flex gap-1">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="w-5 h-5 rounded-[4px] bg-inherit hover:bg-secondary"
-                >
-                  <LeftIcon />
-                </Button>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="w-5 h-5 rounded-[4px] bg-inherit hover:bg-secondary"
-                >
-                  <RightIcon />
-                </Button>
-              </div>
-            </div>
-            <div className="flex flex-col-reverse gap-5">
-              <Button
-                variant="outline"
-                stretch
-                className="rounded-[0.5rem] bg-inherit flex-shrink-0"
-              >
-                Generate New Variations<span>(1/3)</span>
-              </Button>
-              <div className="grid-area-variations gap-4 flex-1">
-                <div className="rounded-[0.5rem] overflow-hidden">
-                  <Image src={Img1} alt="" objectFit="cover" />
-                </div>
-                <div className="rounded-[0.5rem] overflow-hidden">
-                  <Image src={Img1} alt="" />
-                </div>
-                <div className="rounded-[0.5rem] overflow-hidden">
-                  <Image src={Img1} alt="" />
-                </div>
-                <div className="rounded-[0.5rem] overflow-hidden">
-                  <Image src={Img1} alt="" />
-                </div>
-              </div>
-            </div>
+            <VariationsSection />
           </div>
 
           {/* options - Skin Texture */}
@@ -114,8 +79,8 @@ function Model({ modelName }: { modelName: string }) {
             <h3 className="mb-5">Skin Texture</h3>
             <RadioGroup
               id="skinTexture"
-              value={query.skinTexture}
-              onValueChange={handleValueChange}
+              value={skinTexture}
+              onValueChange={handleSkinTextureChange}
             >
               {skinTextureOptions.map((option) => (
                 <RadioGroupItem key={option} value={option} label={option} />
@@ -128,8 +93,8 @@ function Model({ modelName }: { modelName: string }) {
             <h3 className="mb-5">Aspect Ratio</h3>
             <RadioGroup
               id="aspectRatio"
-              value={query.aspectRatio}
-              onValueChange={handleValueChange}
+              value={aspectRatio}
+              onValueChange={handleAspectRatioChange}
             >
               {aspectRatioOptions.map((option) => (
                 <RadioGroupItem key={option} value={option} label={option} />
@@ -142,8 +107,8 @@ function Model({ modelName }: { modelName: string }) {
             <h3 className="mb-5">Face Angle</h3>
             <RadioGroup
               id="faceAngle"
-              value={query.faceAngle}
-              onValueChange={handleValueChange}
+              value={faceAngle}
+              onValueChange={handleFaceAngleChange}
             >
               {faceAngleOptions.map((option) => (
                 <RadioGroupItem key={option} value={option} label={option} />
