3d99139fd83b8688ea001e802d019aef9b268770
ebogyeong <bogyeong.lee@growdle.com>
2024-08-14 08:00:08 +0900

    refactor: update Card component storybook

--- src/shared/ui/Card/index.stories.tsx
+++ src/shared/ui/Card/index.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Card } from '.'
-import BG1 from '/public/images/model-gen-1.png'
 
 const meta: Meta<typeof Card> = {
   component: Card,
@@ -14,5 +13,5 @@ export default meta
 type Story = StoryObj<typeof Card>
 
 export const Default: Story = {
-  args: { imgUrl: BG1, id: 'modelname' }
+  // TODO:
 }
