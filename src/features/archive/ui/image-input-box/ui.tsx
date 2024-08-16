578acc74731feda5510e80c8e3cc766129902a97
ebogyeong <bogyeong.lee@growdle.com>
2024-08-16 11:22:56 +0900

    refactor: add resetImagePreviewUrls to dependency array in imageinputbox component

--- src/features/archive/ui/image-input-box/ui.tsx
+++ src/features/archive/ui/image-input-box/ui.tsx
@@ -29,7 +29,7 @@ export const ImageInputBox = ({
     return () => {
       resetImagePreviewUrls()
     }
-  }, [])
+  }, [resetImagePreviewUrls])
 
   const convertFileToFormData = (file: File) => {
     const formData = new FormData()
