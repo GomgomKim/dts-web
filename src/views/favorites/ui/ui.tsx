dd2de226a6fe90ca5c607a0173892318133dde47
ebogyeong <bogyeong.lee@growdle.com>
2024-08-20 12:54:10 +0900

    feature: add suspense

--- src/views/favorites/ui/ui.tsx
+++ src/views/favorites/ui/ui.tsx
@@ -1,10 +1,13 @@
 import { FavoriteList } from '@/features/favorites/favorites-list'
+import { Suspense } from 'react'
 
 function Favorites() {
   return (
     <>
       <h1 className="text-xl mb-8 text-2xl font-semibold">Favorites</h1>
-      <FavoriteList />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FavoriteList />
+      </Suspense>
     </>
   )
 }
