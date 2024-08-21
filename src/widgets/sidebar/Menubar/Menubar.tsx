2d8ac9ab0316b27ca52c49e59536b9083d7e7a05
ebogyeong <bogyeong.lee@growdle.com>
2024-08-21 18:44:15 +0900

    refactor: remove archive in menubar

--- src/widgets/sidebar/Menubar/Menubar.tsx
+++ src/widgets/sidebar/Menubar/Menubar.tsx
@@ -1,22 +1,22 @@
 import ExploreIcon from '/public/icons/compass.svg'
 import FavoriteIcon from '/public/icons/heart.svg'
-import ArchiveIcon from '/public/icons/folder.svg'
+// import ArchiveIcon from '/public/icons/folder.svg'
 
 import { type MenuItem as MenuItemT } from '@/shared/ui/menubar/model'
-import { Badge, Menu, MenuItem } from '@/shared/ui'
+import { Menu, MenuItem } from '@/shared/ui'
 
 const Menubar = () => {
   // TODO: 로그인 유저
   const navList: MenuItemT[] = [
     { href: '/explore', title: 'Explore', prefix: <ExploreIcon /> },
-    { href: '/favorites', title: 'Favorites', prefix: <FavoriteIcon /> },
-    {
-      href: '/archive',
-      title: 'Archive',
-      prefix: <ArchiveIcon />,
-      postfix: <Badge>Upcoming</Badge>,
-      disabled: true
-    }
+    { href: '/favorites', title: 'Favorites', prefix: <FavoriteIcon /> }
+    // {
+    //   href: '/archive',
+    //   title: 'Archive',
+    //   prefix: <ArchiveIcon />,
+    //   postfix: <Badge>Upcoming</Badge>,
+    //   disabled: true
+    // }
   ]
 
   return (
