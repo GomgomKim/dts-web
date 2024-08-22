e30cb8ac840a1a477997ab44f99aa5fecf57dc85
ebogyeong <bogyeong.lee@growdle.com>
2024-08-22 13:22:27 +0900

    feature: add optimistic update in favorites page

--- src/features/favorites/LikeButton/adapter.ts
+++ src/features/favorites/LikeButton/adapter.ts
@@ -1,51 +1,60 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import {
-  DeleteFavoriteRemoveReqData,
-  PostFavoriteAddReqData
-} from '@/entities/LikeButton/model'
-import {
-  deleteFavoriteRemove,
-  postFavoriteAdd
-} from '@/entities/LikeButton/api'
-
-const usePostFavoriteAdd = () => {
-  return useMutation({
-    mutationFn: ({ encodedBaseImageId }: PostFavoriteAddReqData) =>
-      postFavoriteAdd({ encodedBaseImageId })
-  })
-}
+import { DeleteFavoriteRemoveReqData } from '@/entities/LikeButton/model'
+import { deleteFavoriteRemove } from '@/entities/LikeButton/api'
+import { useSearchParams } from 'next/navigation'
+import { GetFavoriteListResData } from '../FavoriteList/model'
 
 const useDeleteFavoriteRemove = () => {
   const queryClient = useQueryClient()
+
+  const searchParams = useSearchParams()
+  const tagType = searchParams.get('tagType')
+  const queryKey = ['favorites', tagType]
 
   return useMutation({
     mutationFn: ({ encodedBaseImageId }: DeleteFavoriteRemoveReqData) =>
       deleteFavoriteRemove({ encodedBaseImageId }),
     onMutate: async ({ encodedBaseImageId }) => {
       await queryClient.cancelQueries({
-        queryKey: ['favorites']
+        queryKey: queryKey
       })
-      const previousData = queryClient.getQueryData(['favorites'])
 
-      queryClient.setQueryData(['favorites'], (oldData) => ({
-        ...oldData,
-        pages: oldData.pages.map((page) => ({
-          ...page,
-          items: page.items.filter(
-            (item) => item.encodedBaseImageId !== encodedBaseImageId
-          )
-        }))
-      }))
+      const previousData = queryClient.getQueryData<{
+        pages: GetFavoriteListResData[]
+        pageParams: string[]
+      }>(queryKey)
+
+      queryClient.setQueryData(
+        queryKey,
+        (oldData: typeof previousData | undefined) => {
+          if (!oldData) return oldData
+
+          return {
+            ...oldData,
+            pages: oldData.pages.map((page) => ({
+              ...page,
+              content: {
+                ...page.content,
+                images: page.content.images.filter(
+                  (item) => item.encodedBaseImageId !== encodedBaseImageId
+                )
+              }
+            }))
+          }
+        }
+      )
 
       return { previousData }
     },
-    onError: (_error, _variables, context) => {
-      queryClient.setQueryData(['favorites'], context?.previousData)
+    onError: (error, _variables, context) => {
+      queryClient.setQueryData(queryKey, context?.previousData)
+      console.log('failed to delete favorite')
+      console.error(error)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: queryKey })
     }
-    // onSettled: () => {
-    //   queryClient.invalidateQueries({queryKey: ['favorites']})
-    // }
   })
 }
 
-export { usePostFavoriteAdd, useDeleteFavoriteRemove }
+export { useDeleteFavoriteRemove }
--- src/features/favorites/LikeButton/ui.tsx
+++ src/features/favorites/LikeButton/ui.tsx
@@ -1,54 +1,19 @@
 'use client'
 
 import * as entities from '@/entities/LikeButton'
-import {
-  useDeleteFavoriteRemove,
-  usePostFavoriteAdd
-} from '@/entities/LikeButton/adapter'
-import { useQueryClient } from '@tanstack/react-query'
-import { useState } from 'react'
-import { useSearchParams } from 'next/navigation'
+import { useDeleteFavoriteRemove } from './adapter'
 
 export type Props = {
   id: string
-  isFavorite: boolean
   className?: string
 }
 
 export const LikeButton = (props: Props) => {
-  const queryClient = useQueryClient()
-
-  const searchParams = useSearchParams()
-  const [isActive, setIsActive] = useState(true)
-
-  // TODO: hook으로 빼기 + 낙관적 업데이트 적용
-  const postFavoriteAdd = usePostFavoriteAdd()
   const deleteFavoriteRemove = useDeleteFavoriteRemove()
 
   const handleClick = () => {
-    const tagType = searchParams.get('tagType')
-
-    if (isActive) {
-      deleteFavoriteRemove.mutate(
-        { encodedBaseImageId: props.id },
-        {
-          onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['favorites', tagType] })
-            setIsActive((prev) => !prev)
-          }
-        }
-      )
-    } else {
-      postFavoriteAdd.mutate(
-        { encodedBaseImageId: props.id },
-        {
-          onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['favorites', tagType] })
-            setIsActive((prev) => !prev)
-          }
-        }
-      )
-    }
+    deleteFavoriteRemove.mutate({ encodedBaseImageId: props.id })
   }
-  return <entities.LikeButton handleClick={handleClick} isActive={isActive} />
+
+  return <entities.LikeButton handleClick={handleClick} isActive={true} />
 }
