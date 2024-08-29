93f77824c98327ab7240f0141e1026e5962516f7
ebogyeong <bogyeong.lee@growdle.com>
2024-08-29 09:06:11 +0900

    refactor: move app/explore routing logic to views/explore

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,44 +1,12 @@
 'use client'
 
-import { useAuthStore } from '@/entities/user/store'
-import { dtsAxios } from '@/shared/api'
 import Explore from '@/views/explore/ui'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { Suspense } from 'react'
 
 export default function Page() {
-  const searchParams = useSearchParams()
-  const router = useRouter()
-
-  const logIn = useAuthStore((state) => state.logIn)
-  const isAuth = useAuthStore((state) => state.isAuth)
-
-  useEffect(() => {
-    if (isAuth) return
-
-    if (searchParams.has('oAuthProviderType') && searchParams.has('code')) {
-      const getTokens = async () => {
-        const response = await dtsAxios.get('/auth/access-token', {
-          params: {
-            oAuthProviderType: searchParams.get('oAuthProviderType'),
-            code: searchParams.get('code')
-          }
-        })
-        return response.headers
-      }
-
-      getTokens().then((headers) => {
-        logIn({
-          accessToken: headers['authorization'],
-          refreshToken: headers['refresh-token']
-        })
-      })
-
-      router.replace('/explore')
-    } else {
-      console.log('not logged in ')
-    }
-  }, [searchParams])
-
-  return <Explore />
+  return (
+    <Suspense fallback={<div>Loading explore page...</div>}>
+      <Explore />
+    </Suspense>
+  )
 }
