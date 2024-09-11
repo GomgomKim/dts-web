677672f46b2c5e8a7245523af33d7d5815eabdfa
ebogyeong <bogyeong.lee@growdle.com>
2024-09-12 08:32:17 +0900

    feature: add get auth profile in explore page

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -5,12 +5,20 @@ import * as React from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useAuthStore } from '@/entities/user/store'
 import { dtsAxios } from '@/shared/api'
+import {
+  GetAuthProfileReqData,
+  GetAuthProfileResData
+} from '@/entities/user/model'
+import { AxiosError, AxiosResponse } from 'axios'
+import { URL_AUTH_PROFILE } from '@/entities/user/constant'
+import { Tokens } from '@/entities/user/type'
 
 export default function Page() {
   const searchParams = useSearchParams()
   const router = useRouter()
 
-  const { logIn, isAuth } = useAuthStore.getState()
+  // 이렇게 하면 무조건 explore 페이지로 리다이렉션?
+  const { logIn, isAuth, setUser, setRestriction } = useAuthStore.getState()
 
   React.useEffect(() => {
     // already logged in
@@ -30,6 +38,7 @@ export default function Page() {
     // 로그인 직후
     if (searchParams.has('oAuthProviderType') && searchParams.has('code')) {
       const getTokens = async () => {
+        // try {
         const response = await dtsAxios.get('/auth/access-token', {
           params: {
             oAuthProviderType: searchParams.get('oAuthProviderType'),
@@ -37,17 +46,49 @@ export default function Page() {
           }
         })
         return response.headers
+        // } catch (error) {
+        //   console.error('Error fetching tokens:', error)
+        // }
+      }
+
+      const getUserProfile = async (token: Tokens) => {
+        //   try {
+        const response = await dtsAxios.get<
+          GetAuthProfileReqData,
+          AxiosResponse<GetAuthProfileResData, AxiosError>
+        >(URL_AUTH_PROFILE, {
+          headers: {
+            Authorization: token.accessToken,
+            'Refresh-Token': token.refreshToken
+          }
+        })
+        return response.data
+        //   } catch (error) {
+        //     console.error('Error fetching user profile:', error)
+        //   }
       }
 
       getTokens().then((headers) => {
-        logIn({
-          accessToken: headers['authorization'],
-          refreshToken: headers['refresh-token']
-        })
+        if (headers) {
+          const token = {
+            accessToken: headers['authorization'],
+            refreshToken: headers['refresh-token']
+          }
+
+          logIn(token)
+
+          getUserProfile(token).then((res) => {
+            if (res) {
+              const { email, profileImageUrl, restriction } = res.content
+              setUser({ email, profileImageUrl })
+              setRestriction(restriction)
+            }
+          })
+          router.replace('/explore')
+        }
       })
-      router.replace('/explore')
     }
-  }, [searchParams])
+  }, [searchParams, isAuth])
 
   return <Explore />
 }
