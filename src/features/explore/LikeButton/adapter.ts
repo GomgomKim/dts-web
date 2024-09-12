3672f516bc0a6afc04a8075e2e6a502ae3f744ee
ebogyeong <bogyeong.lee@growdle.com>
2024-09-12 19:37:49 +0900

    refactor: add optimistic update and remove invalidatequeries in like button

--- src/features/explore/LikeButton/adapter.ts
+++ src/features/explore/LikeButton/adapter.ts
@@ -10,70 +10,76 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useSearchParams } from 'next/navigation'
 import { GetExploreListResData } from '../CardList/model'
 import { FILTER_TYPES } from '@/features/favorites/FavoriteList/constant'
+import { useFilterTypeStore } from '@/shared/lib/stores/useFilterTypeStore'
 
 // TODO: invalidate 작동 후 스크롤 유지
 
 const usePostFavoriteAdd = () => {
   const queryClient = useQueryClient()
+  const { filterType: previousFilterType } = useFilterTypeStore.getState()
 
   const searchParams = useSearchParams()
-  const filterType = searchParams.get('filterType') || FILTER_TYPES[0]
+  const filterType =
+    searchParams.get('filterType') || previousFilterType || FILTER_TYPES[0]
   const queryKey = ['explore', filterType]
 
   return useMutation({
     mutationFn: ({ encodedImageInfoId }: PostFavoriteAddReqData) =>
       postFavoriteAdd({ encodedImageInfoId }),
-    // onMutate: async ({ encodedBaseImageId }) => {
-    //   await queryClient.cancelQueries({
-    //     queryKey: queryKey
-    //   })
+    onMutate: async ({ encodedImageInfoId }) => {
+      await queryClient.cancelQueries({
+        queryKey: queryKey
+      })
 
-    //   const previousData = queryClient.getQueryData<{
-    //     pages: GetExploreListResData[]
-    //     pageParams: string[]
-    //   }>(queryKey)
+      const previousData = queryClient.getQueryData<{
+        pages: GetExploreListResData[]
+        pageParams: string[]
+      }>(queryKey)
 
-    //   queryClient.setQueryData(
-    //     queryKey,
-    //     (oldData: typeof previousData | undefined) => {
-    //       if (!oldData) return oldData
+      queryClient.setQueryData(
+        queryKey,
+        (oldData: typeof previousData | undefined) => {
+          if (!oldData) return oldData
 
-    //       return {
-    //         ...oldData,
-    //         pages: oldData.pages.map((page) => ({
-    //           ...page,
-    //           content: {
-    //             ...page.content,
-    //             images: page.content.images.map((item) => {
-    //               if (item.encodedBaseImageId === encodedBaseImageId)
-    //                 return { ...item, isFavorite: true }
-    //               return item
-    //             })
-    //           }
-    //         }))
-    //       }
-    //     }
-    //   )
+          return {
+            ...oldData,
+            pages: oldData.pages.map((page) => ({
+              ...page,
+              content: {
+                ...page.content,
+                images: page.content.images.map((item) => {
+                  if (item.encodedImageInfoId === encodedImageInfoId) {
+                    return { ...item, isFavorite: true }
+                  }
+                  return item
+                })
+              }
+            }))
+          }
+        }
+      )
 
-    //   return { previousData }
-    // },
+      return { previousData }
+    },
     onError: (error) => {
       // onError: (error, _variables, context) => {
       //   queryClient.setQueryData(queryKey, context?.previousData)
       console.log('failed to add favorite in explore page')
       console.error(error)
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: queryKey })
     }
+    // onSettled: () => {
+    //   queryClient.invalidateQueries({ queryKey: queryKey })
+    // }
   })
 }
 
 const useDeleteFavoriteRemove = () => {
   const queryClient = useQueryClient()
+  const { filterType: previousFilterType } = useFilterTypeStore.getState()
 
   const searchParams = useSearchParams()
-  const filterType = searchParams.get('filterType') || FILTER_TYPES[0]
+  const filterType =
+    searchParams.get('filterType') || previousFilterType || FILTER_TYPES[0]
   const queryKey = ['explore', filterType]
 
   return useMutation({
@@ -117,10 +123,10 @@ const useDeleteFavoriteRemove = () => {
       queryClient.setQueryData(queryKey, context?.previousData)
       console.log('failed to delete favorite in explore page')
       console.error(error)
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: queryKey })
     }
+    // onSettled: () => {
+    //   queryClient.invalidateQueries({ queryKey: queryKey })
+    // }
   })
 }
 
