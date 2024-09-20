ab883305e794ce139890bd3f169732269ebad418
ebogyeong <bogyeong.lee@growdle.com>
2024-09-20 14:22:46 +0900

    feature: update variation options schema

--- src/entities/detail/constant.ts
+++ src/entities/detail/constant.ts
@@ -8,27 +8,18 @@ export const URL_AI_IMAGE_GENERATE_PROGRESS = '/image/ai-image/progress'
 export const URL_ASSET_REMOVE_BACKGROUND = '/image-file/asset/remove_background'
 
 export const ASPECT_RATIO_MAP: Record<AspectRatio, string> = {
-  ASPECT_RATIO_16_9: '16:9',
   ASPECT_RATIO_9_16: '9:16',
-  ASPECT_RATIO_1_1: '1:1',
-  ASPECT_RATIO_4_3: '4:3',
-  ASPECT_RATIO_3_4: '3:4'
+  ASPECT_RATIO_1_1: '1:1'
 }
 
 export const ASPECT_RATIO_REVERT_MAP: Record<string, AspectRatio> = {
-  '16:9': 'ASPECT_RATIO_16_9',
   '9:16': 'ASPECT_RATIO_9_16',
-  '1:1': 'ASPECT_RATIO_1_1',
-  '4:3': 'ASPECT_RATIO_4_3',
-  '3:4': 'ASPECT_RATIO_3_4'
+  '1:1': 'ASPECT_RATIO_1_1'
 }
 
 export const ASPECT_RATIO_MAP_NUMBER: Record<AspectRatio, number> = {
-  ASPECT_RATIO_16_9: 16 / 9,
-  ASPECT_RATIO_4_3: 4 / 3,
-  ASPECT_RATIO_1_1: 1 / 1,
-  ASPECT_RATIO_3_4: 3 / 4,
-  ASPECT_RATIO_9_16: 9 / 16
+  ASPECT_RATIO_9_16: 9 / 16,
+  ASPECT_RATIO_1_1: 1 / 1
 }
 
 export const FACE_ANGLE_MAP: Record<FaceAngle, string> = {
