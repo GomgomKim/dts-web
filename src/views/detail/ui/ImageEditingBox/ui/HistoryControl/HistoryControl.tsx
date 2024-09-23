e0f43e8c0a2108ee81c231dd00a1863697794034
ebogyeong <bogyeong.lee@growdle.com>
2024-09-23 16:28:24 +0900

    feature: add handleUndo, handleRedo in HistoryControl

--- src/views/detail/ui/ImageEditingBox/ui/HistoryControl/HistoryControl.tsx
+++ src/views/detail/ui/ImageEditingBox/ui/HistoryControl/HistoryControl.tsx
@@ -1,18 +1,57 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+import { useEditorStore } from '@/views/detail/model/useEditorHistoryStore'
+
 import { Button } from '@/shared/ui'
 
 import UndoIcon from '/public/icons/corner-up-left.svg'
 import RestoreIcon from '/public/icons/refresh.svg'
 
 export const HistoryControl = () => {
-  const hasFuture = true
-  const hasPast = true
+  const searchParams = useSearchParams()
+
+  const variationId = searchParams.get('variationId')
+
+  const editedVariationList = useEditorStore((state) => state.items)
+  const undo = useEditorStore((state) => state.undo)
+  const redo = useEditorStore((state) => state.redo)
+
+  if (!variationId) {
+    console.log('variationId is not found')
+    return null
+  }
+
+  const hasPast = editedVariationList.has(variationId)
+    ? editedVariationList.get(variationId)!.past.length > 0
+    : false
+  const hasFuture = editedVariationList.has(variationId)
+    ? editedVariationList.get(variationId)!.future.length > 0
+    : false
+
+  const handleUndo = () => {
+    undo(variationId)
+  }
+
+  const handleRedo = () => {
+    redo(variationId)
+  }
+
+  // TODO: restore 기능 구현
+  const handleRestore = () => {
+    console.log('restore clicked')
+    // present가 초기값이면 적용하지 않음
+    // present가 초기값이 아니면 초기값으로 되돌림
+  }
+
   return (
     <div className="flex gap-1">
       <Button
         variant="ghost"
         size="icon"
         className="bg-inherit h-8 w-8 rounded-[0.25rem]"
-        onClick={() => console.log('undo clicked')}
+        onClick={handleUndo}
         disabled={!hasPast}
       >
         <UndoIcon stroke="#AEAFB5" />
@@ -21,7 +60,7 @@ export const HistoryControl = () => {
         variant="ghost"
         size="icon"
         className="bg-inherit h-8 w-8 rounded-[0.25rem]"
-        onClick={() => console.log('redo clicked')}
+        onClick={handleRedo}
         disabled={!hasFuture}
       >
         <UndoIcon stroke="#AEAFB5" className="-scale-x-100" />
@@ -30,7 +69,8 @@ export const HistoryControl = () => {
         variant="ghost"
         size="icon"
         className="bg-inherit h-8 w-8 rounded-[0.25rem]"
-        onClick={() => console.log('restore clicked')}
+        onClick={handleRestore}
+        // present가 초기값이면 disabled
       >
         <RestoreIcon stroke="#AEAFB5" />
       </Button>
