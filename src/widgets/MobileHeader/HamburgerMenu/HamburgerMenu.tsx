d606e90d45a2abee9d1ef75b92b80f6fff041711
ebogyeong <bogyeong.lee@growdle.com>
2024-09-30 17:26:15 +0900

    feature: design menubar in mobile

--- src/widgets/MobileHeader/HamburgerMenu/HamburgerMenu.tsx
+++ src/widgets/MobileHeader/HamburgerMenu/HamburgerMenu.tsx
@@ -2,12 +2,27 @@
 
 import * as React from 'react'
 
+import Link from 'next/link'
+
+import { useAuthStore } from '@/entities/UserProfile/store'
+import { Instructions } from '@/entities/mobile/ui/Instructions'
+
+import { cn } from '@/shared/lib/utils'
 import { Button } from '@/shared/ui'
 
 import HamburgerIcon from '/public/icons/hamburger.svg'
+import XIcon from '/public/icons/x.svg'
+
+import { useQueryClient } from '@tanstack/react-query'
 
 export const HamburgerMenu = () => {
+  const queryClient = useQueryClient()
+
   const [isOpen, setIsOpen] = React.useState(false)
+
+  const isAuth = useAuthStore((state) => state.isAuth)
+  const logOut = useAuthStore((state) => state.logOut)
+
   return (
     <>
       <Button
@@ -15,15 +30,64 @@ export const HamburgerMenu = () => {
         size="icon"
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        {isOpen ? 'x' : <HamburgerIcon />}
+        {isOpen ? <XIcon /> : <HamburgerIcon />}
       </Button>
-      {isOpen ? (
-        <div className="fixed inset-0 top-14">
-          <div className="absolute top-0 right-0 w-3/4 h-full z-[20] bg-background p-5 animate-sidebar-fadeIn">
-            navbar
-          </div>
+      <div
+        className={cn(
+          'fixed inset-0 z-[30] top-14 h-[calc(100vh-64px)] bg-background transition-transform duration-300 ease-in-out',
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        )}
+      >
+        <div className="px-5 py-8">
+          <Instructions />
         </div>
-      ) : null}
+        <nav className="flex flex-col">
+          <ul>
+            <li>
+              <Button
+                asChild
+                variant="link"
+                className="py-[24px] text-[1.25rem]"
+              >
+                <Link
+                  href="https://medium.com/do-things-with-ai"
+                  target="_blank"
+                >
+                  Medium
+                </Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                asChild
+                variant="link"
+                className="py-[24px] text-[1.25rem]"
+              >
+                <Link
+                  href="https://www.instagram.com/dothings.studio/"
+                  target="_blank"
+                >
+                  Instagram
+                </Link>
+              </Button>
+            </li>
+            {isAuth ? (
+              <li>
+                <Button
+                  variant="link"
+                  className="py-[24px] text-[1.25rem]"
+                  onClick={() => {
+                    setIsOpen(false)
+                    logOut(queryClient)
+                  }}
+                >
+                  Log Out
+                </Button>
+              </li>
+            ) : null}
+          </ul>
+        </nav>
+      </div>
     </>
   )
 }
