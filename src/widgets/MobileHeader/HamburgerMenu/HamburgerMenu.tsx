5ac0dd6bce18a58683d7ac6f4e3bab70d4c3165d
ebogyeong <bogyeong.lee@growdle.com>
2024-09-30 14:14:51 +0900

    feature: mobile menubar animate

--- src/widgets/MobileHeader/HamburgerMenu/HamburgerMenu.tsx
+++ src/widgets/MobileHeader/HamburgerMenu/HamburgerMenu.tsx
@@ -9,12 +9,21 @@ import HamburgerIcon from '/public/icons/hamburger.svg'
 export const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = React.useState(false)
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => setIsOpen((prev) => !prev)}
-    >
-      {isOpen ? 'x' : <HamburgerIcon />}
-    </Button>
+    <>
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {isOpen ? 'x' : <HamburgerIcon />}
+      </Button>
+      {isOpen ? (
+        <div className="fixed inset-0 top-14">
+          <div className="absolute top-0 right-0 w-3/4 h-full z-[20] bg-background p-5 animate-sidebar-fadeIn">
+            navbar
+          </div>
+        </div>
+      ) : null}
+    </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,9 +72,14 @@ const config = {
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' }
+        },
+        'sidebar-fadeIn': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
         }
       },
       animation: {
+        'sidebar-fadeIn': 'sidebar-fadeIn 0.2s ease-in-out',
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
       },
