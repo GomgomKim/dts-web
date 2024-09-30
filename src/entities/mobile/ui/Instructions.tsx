64e3146da502e5f072438a996a42079c68f690e3
ebogyeong <bogyeong.lee@growdle.com>
2024-09-30 13:38:26 +0900

    feature: add Instructions ui in mobile

---
+++ src/entities/mobile/ui/Instructions.tsx
@@ -0,0 +1,12 @@
+export const Instructions = () => {
+  return (
+    <div>
+      <h2 className="text-nowrap text-[1.125rem]">
+        Currently not supported on mobile.
+      </h2>
+      <h3 className="text-nowrap text-[1.125rem] font-normal text-neutral-5">
+        Please use a desktop, laptop, or tablet.
+      </h3>
+    </div>
+  )
+}
