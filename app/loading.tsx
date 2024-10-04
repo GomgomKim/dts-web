9b0568bb70decbaf2c8df7aeee476d2820ea5597
ebogyeong <bogyeong.lee@growdle.com>
2024-10-04 13:47:13 +0900

    feature: add loading spinner for initial page load

---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingSpinner } from '@/shared/ui/LoadingSpinner/LoadingSpinner'
+
+export default function Loading() {
+  return <LoadingSpinner width="40" height="40" />
+}
---
+++ src/shared/ui/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+import Spinner from '/public/icons/loading-spinner.svg'
+
+interface LoadingSpinnerProps {
+  width: string
+  height: string
+}
+
+export const LoadingSpinner = ({ width, height }: LoadingSpinnerProps) => {
+  return (
+    <div className={`w-[${width}]} h-[${height}] absolute-center`}>
+      <Spinner className="animate-spin" width={width} height={height} />
+    </div>
+  )
+}
