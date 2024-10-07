c22c9ab65c15a4797ece78f26e41bd310dc0bd80
ebogyeong <bogyeong.lee@growdle.com>
2024-10-07 16:35:29 +0900

    refactor: replace after mount in root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 
+import * as React from 'react'
+
 import { useRouter } from 'next/navigation'
 
 export default function Page() {
   const router = useRouter()
-  router.replace('/explore?filterType=ALL')
+
+  React.useEffect(() => {
+    router.replace('/explore?filterType=ALL')
+  }, [router])
+
+  return null
 }
