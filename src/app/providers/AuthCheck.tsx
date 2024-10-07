1f4005b443f460be76b3b826042c26147800698a
ebogyeong <bogyeong.lee@growdle.com>
2024-10-07 17:34:10 +0900

    feature: check auth null in AuthCheck component

--- src/app/providers/AuthCheck.tsx
+++ src/app/providers/AuthCheck.tsx
@@ -6,24 +6,39 @@ import { useRouter } from 'next/navigation'
 
 import { useAuthStore } from '@/entities/UserProfile/store'
 
-export default function AuthCheck({ children }: { children: React.ReactNode }) {
+import { LoadingSpinner } from '@/shared/ui/LoadingSpinner'
+
+interface AuthCheckProps {
+  children: React.ReactNode
+  routePath?: string
+}
+
+export default function AuthCheck({ children, routePath }: AuthCheckProps) {
   const isAuth = useAuthStore((state) => state.isAuth)
+  const setIsAuth = useAuthStore((state) => state.setIsAuth)
+
   const router = useRouter()
+
   const [loading, setLoading] = React.useState(true)
 
   React.useEffect(() => {
     if (isAuth === null) {
+      const history = localStorage.getItem('dts-auth-store')
+
+      if (!history) setIsAuth(false)
+
       setLoading(true)
     } else {
-      setLoading(false)
       if (!isAuth) {
-        router.replace('/login')
+        router.replace(routePath ? routePath : '/login')
       }
+
+      setLoading(false)
     }
   }, [isAuth, router])
 
   if (loading) {
-    return <div>auth checking...</div>
+    return <LoadingSpinner width="40" height="40" />
   }
 
   if (!isAuth) {
