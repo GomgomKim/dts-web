0777a1a1673e1bb1d2251c16ca3681b958283ae9
ebogyeong <bogyeong.lee@growdle.com>
2024-10-07 16:24:57 +0900

    refactor: update createQueryStrings dependency

--- src/shared/lib/hooks/useSetQueryString.ts
+++ src/shared/lib/hooks/useSetQueryString.ts
@@ -16,18 +16,21 @@ export const useSetQueryString = (props: useSetQueryStringParams) => {
   const pathname = usePathname()
   const params = new URLSearchParams(searchParams.toString())
 
-  const createQueryStrings = useCallback((queryParams: QueryParams[]) => {
-    queryParams.forEach((param) => {
-      Object.entries(param).forEach(([key, value]) => {
-        if (value) {
-          params.set(key, value)
-        } else {
-          params.delete(key)
-        }
+  const createQueryStrings = useCallback(
+    (queryParams: QueryParams[]) => {
+      queryParams.forEach((param) => {
+        Object.entries(param).forEach(([key, value]) => {
+          if (value) {
+            params.set(key, value)
+          } else {
+            params.delete(key)
+          }
+        })
       })
-    })
-    return params.toString()
-  }, [])
+      return params.toString()
+    },
+    [searchParams]
+  )
 
   const handleQueryString = (queryParams: QueryParams[]) => {
     const queryString = createQueryStrings(queryParams)
