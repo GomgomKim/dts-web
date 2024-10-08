5bf203f99043b5ae09440eec957097ade9b3f335
ebogyeong <bogyeong.lee@growdle.com>
2024-10-08 16:53:09 +0900

    refactor: separate logic and ui components in ImageInputBox

--- src/views/detail/ui/BrandAssets/ui/ImageInputBox/ImageInputBox.tsx
+++ src/views/detail/ui/BrandAssets/ui/ImageInputBox/ImageInputBox.tsx
@@ -5,19 +5,16 @@ import * as React from 'react'
 import { useImagePreviewUrlStore } from '@/entities/detail/store'
 
 import { DndBox } from '@/shared/lib/hocs/DndBox'
-import { Button } from '@/shared/ui/button'
 
 import DashedSvg from '/public/icons/dashed.svg'
-import DeleteIcon from '/public/icons/delete.svg'
-import SpinnerIcon from '/public/icons/loading-spinner.svg'
 
-import { usePostRemoveBackground } from './model/adapter'
-
-const IMAGE_MAX_SIZE = 5 * 1024 * 1024
-
-const isValidImageSize = (file: File) => {
-  return file.size > IMAGE_MAX_SIZE ? false : true
-}
+import { useImageInputBox } from './model/useImageInputBox'
+import {
+  ErrorInstruction,
+  LoadingInstruction,
+  RemoveButton,
+  UploadButton
+} from './ui'
 
 interface ImageInputBoxProps {
   boxId: string
@@ -27,84 +24,14 @@ interface ImageInputBoxProps {
 export const ImageInputBox = (props: ImageInputBoxProps) => {
   const [errorMessage, setErrorMessage] = React.useState<string | null>(null)
 
-  const {
-    imagePreviewUrls,
-    addImagePreviewUrl,
-    removeImagePreviewUrl,
-    resetImagePreviewUrls
-  } = useImagePreviewUrlStore()
+  const { handleChangeDNDInput, handleChangeInput, isPending } =
+    useImageInputBox({
+      boxId: props.boxId,
+      onChangeBrandAsset: props.onChangeBrandAsset,
+      setErrorMessage
+    })
 
-  const removeBackgroundMutation = usePostRemoveBackground()
-
-  React.useEffect(() => {
-    return () => {
-      resetImagePreviewUrls()
-    }
-  }, [resetImagePreviewUrls])
-
-  const convertFileToFormData = (file: File) => {
-    const formData = new FormData()
-    formData.append('source', file)
-    return formData
-  }
-
-  const handleSubmit = ({ formData }: { formData: FormData }) => {
-    setErrorMessage(null)
-
-    removeBackgroundMutation.mutate(
-      { source: formData },
-      {
-        onSuccess: (data) => {
-          const blob = new Blob([data], { type: 'image/png' })
-          addImagePreviewUrl(props.boxId, blob)
-        },
-        onError: () => {
-          setErrorMessage('Oops! Try again.')
-        }
-      }
-    )
-  }
-
-  const handleChangeImageFile = (file: File) => {
-    const formData = convertFileToFormData(file)
-    handleSubmit({ formData })
-    // TODO: remove this code after the API is ready
-    // const blob = new Blob([file], { type: 'image/png' })
-    // addImagePreviewUrl(props.boxId, blob)
-  }
-
-  const handleChangeInput = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = e.target
-    if (!files) return
-
-    if (!isValidImageSize(files[0])) {
-      setErrorMessage('Check the file size or format.')
-      e.target.value = ''
-      return
-    } else {
-      setErrorMessage(null)
-    }
-
-    handleChangeImageFile(files[0])
-    e.target.value = ''
-  }
-
-  const handleChangeDNDInput = (file: File) => {
-    if (!isValidImageSize(file)) {
-      setErrorMessage('Check the file size or format.')
-      return
-    } else {
-      setErrorMessage(null)
-    }
-
-    handleChangeImageFile(file)
-    props?.onChangeBrandAsset()
-  }
-
-  const handleClickRemoveButton = () => {
-    removeImagePreviewUrl(props.boxId)
-    props?.onChangeBrandAsset()
-  }
+  const { imagePreviewUrls } = useImagePreviewUrlStore()
 
   const renderContent = () => {
     if (imagePreviewUrls.has(props.boxId)) {
@@ -113,29 +40,24 @@ export const ImageInputBox = (props: ImageInputBoxProps) => {
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={imagePreviewUrls.get(props.boxId)}
-            alt=""
+            alt={props.boxId}
             className="h-full absolute-center"
           />
-          <Button
-            variant="secondary"
-            size="icon"
-            onClick={handleClickRemoveButton}
-            className="absolute top-[1rem] right-[1rem] group"
-          >
-            <DeleteIcon className="stroke-current group-hover:stroke-white" />
-          </Button>
+          <RemoveButton
+            onClickRemoveButton={() => props.onChangeBrandAsset()}
+          />
         </div>
       )
     } else {
       return (
         <>
-          {removeBackgroundMutation.isPending ? (
+          {isPending ? (
             <LoadingInstruction />
           ) : (
             <div className="relative">
-              <UploadButton id={props.boxId} />
+              <UploadButton boxId={props.boxId} />
               {errorMessage !== null ? (
-                <ErrorInstruction errorMessage={errorMessage} />
+                <ErrorInstruction>{errorMessage}</ErrorInstruction>
               ) : null}
             </div>
           )}
@@ -165,43 +87,5 @@ export const ImageInputBox = (props: ImageInputBoxProps) => {
         {renderContent()}
       </DndBox>
     </>
-  )
-}
-
-const LoadingInstruction = () => {
-  return (
-    <div>
-      <div className="w-4 h-4 m-auto mb-2">
-        <SpinnerIcon className="animate-spin" width="16" height="16" />
-      </div>
-      <p className="text-neutral-7 text-nowrap text-[0.875rem]">
-        Tweaking the background...
-      </p>
-    </div>
-  )
-}
-
-const ErrorInstruction = ({ errorMessage }: { errorMessage: string }) => {
-  return (
-    <p className="absolute bottom-[-0.25rem] translate-y-full w-full text-center text-[0.875rem] text-[#FF8480]">
-      {errorMessage}
-    </p>
-  )
-}
-
-const UploadButton = ({ id }: { id: string }) => {
-  return (
-    <div>
-      <div className="mb-3 text-center">
-        <Button asChild variant="outline">
-          <label htmlFor={id} role="button">
-            Upload
-          </label>
-        </Button>
-      </div>
-      <p className="text-neutral-5 whitespace-nowrap text-[0.875rem]">
-        Supports JPG and PNG up to 5MB
-      </p>
-    </div>
   )
 }
