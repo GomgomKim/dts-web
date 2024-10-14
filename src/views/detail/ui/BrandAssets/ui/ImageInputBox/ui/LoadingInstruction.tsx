9abb12d45a5e0516a868939336094ab24f18da10
ebogyeong <bogyeong.lee@growdle.com>
2024-10-14 09:14:14 +0900

    feature: responsive font size in remove bg loading

--- src/views/detail/ui/BrandAssets/ui/ImageInputBox/ui/LoadingInstruction.tsx
+++ src/views/detail/ui/BrandAssets/ui/ImageInputBox/ui/LoadingInstruction.tsx
@@ -6,7 +6,7 @@ export const LoadingInstruction = () => {
       <div className="w-4 h-4 m-auto mb-2">
         <SpinnerIcon className="animate-spin" width="16" height="16" />
       </div>
-      <p className="text-neutral-7 text-nowrap text-[0.875rem]">
+      <p className="text-neutral-7 text-nowrap text-[0.875rem] min-[3840px]:text-[1.25rem]">
         Tweaking the background...
       </p>
     </div>
