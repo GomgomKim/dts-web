ecb5ccfacb5c1b92d15d7ed79d4b6d1b27f334c4
ebogyeong <bogyeong.lee@growdle.com>
2024-10-15 20:40:46 +0900

    feature: move getUserProfile logic from useGetAuthToken to UserProfileSummary

--- src/shared/lib/hooks/useGetAuthToken.ts
+++ src/shared/lib/hooks/useGetAuthToken.ts
@@ -2,17 +2,9 @@ import * as React from 'react'
 
 import { useRouter, useSearchParams } from 'next/navigation'
 
-import { URL_AUTH_PROFILE } from '@/entities/UserProfile/constant'
-import {
-  GetAuthProfileReqData,
-  GetAuthProfileResData
-} from '@/entities/UserProfile/model/types'
 import { useAuthStore } from '@/entities/UserProfile/store'
 
 import { dtsAxios } from '@/shared/api'
-import { Tokens } from '@/shared/api/types'
-
-import { AxiosError, AxiosResponse } from 'axios'
 
 export const useGetAuthToken = ({ redirectPath }: { redirectPath: string }) => {
   const searchParams = useSearchParams()
@@ -20,8 +12,6 @@ export const useGetAuthToken = ({ redirectPath }: { redirectPath: string }) => {
 
   const logIn = useAuthStore((state) => state.logIn)
   const isAuth = useAuthStore((state) => state.isAuth)
-  const setUser = useAuthStore((state) => state.setUser)
-  const setRestriction = useAuthStore((state) => state.setRestriction)
 
   React.useEffect(() => {
     if (isAuth === true) {
@@ -42,14 +32,6 @@ export const useGetAuthToken = ({ redirectPath }: { redirectPath: string }) => {
 
           logIn(token)
 
-          // TODO: 프로필 컴포넌트로 이동 & 프리페치
-          getUserProfile(token).then((res) => {
-            if (res) {
-              const { email, profileImageUrl, restriction } = res.content
-              setUser({ email, profileImageUrl })
-              setRestriction(restriction)
-            }
-          })
           router.replace(redirectPath)
         }
       })
@@ -69,21 +51,4 @@ const getTokens = async (oAuthProviderType: string, code: string) => {
   // } catch (error) {
   //   console.error('Error fetching tokens:', error)
   // }
-}
-
-const getUserProfile = async (token: Tokens) => {
-  //   try {
-  const response = await dtsAxios.get<
-    GetAuthProfileReqData,
-    AxiosResponse<GetAuthProfileResData, AxiosError>
-  >(URL_AUTH_PROFILE, {
-    headers: {
-      Authorization: token.accessToken,
-      'Refresh-Token': token.refreshToken
-    }
-  })
-  return response.data
-  //   } catch (error) {
-  //     console.error('Error fetching user profile:', error)
-  //   }
 }
