d622e70bce3fb53d262309ad2d30baf625d4eea5
ebogyeong <bogyeong.lee@growdle.com>
2024-10-16 12:51:42 +0900

    feature: add mb to explore

--- src/views/explore/index.tsx
+++ src/views/explore/index.tsx
@@ -18,7 +18,7 @@ export default function Explore() {
   return (
     <>
       <CallToActionButtons onClickSeeExample={onMoveToElement} />
-      <div className="max-w-[1136px] min-[3200px]:max-w-[1646px] min-[3840px]:max-w-[2184px] m-auto">
+      <div className="min-h-screen max-w-[1136px] min-[3200px]:max-w-[1646px] min-[3840px]:max-w-[2184px] m-auto pb-[160px]">
         <div className="mb-5 scroll-mt-[56px]" ref={element}>
           <Filter id="explore-filter" filterList={FILTER_TYPES} />
         </div>
