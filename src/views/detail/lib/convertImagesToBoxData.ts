f34cf929b9251e89cd0e868d5f28aefd2750300d
ebogyeong <bogyeong.lee@growdle.com>
2024-10-17 20:33:19 +0900

    refactor: update Box interface and convertImagesToBoxData

--- src/views/detail/lib/convertImagesToBoxData.ts
+++ src/views/detail/lib/convertImagesToBoxData.ts
@@ -1,13 +1,20 @@
 export const convertImagesToBoxData = (assetImages: Map<string, string>) => {
   // 초기 위치로 reset
-  const imageData = Array.from(assetImages.entries()).map(([id, image]) => ({
-    id,
-    createdAt: Date.now(),
-    image,
-    bottom: id === 'logo' ? 40 : 148,
-    height: id === 'logo' ? 100 : 200,
-    width: 200
-  }))
+  const imageData = Array.from(assetImages.entries()).map(([id, image]) => {
+    const width = 200
+    const height = id === 'logo' ? 100 : 200
+    const bottomDistance = id === 'logo' ? 40 : 148
+
+    return {
+      id,
+      createdAt: Date.now(),
+      image,
+      centerX: 0, // 상위 컨테이너 중심에 위치
+      centerY: bottomDistance, // 하단에서부터 위치
+      width,
+      height
+    }
+  })
 
   return imageData
 }
