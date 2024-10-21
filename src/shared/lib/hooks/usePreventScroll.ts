f00f26ad374ef24fd2a994dfe7428630ce3cc0f1
ebogyeong <bogyeong.lee@growdle.com>
2024-10-21 15:32:21 +0900

    feature: fix scroll layout shift bug

--- src/shared/lib/hooks/usePreventScroll.ts
+++ src/shared/lib/hooks/usePreventScroll.ts
@@ -1,10 +1,30 @@
 import { useEffect } from 'react'
 
 export const usePreventScroll = () => {
+  const preventScroll = () => {
+    const currentScrollY = window.scrollY
+
+    document.body.style.position = 'fixed'
+    document.body.style.width = '100%'
+    document.body.style.top = `-${currentScrollY}px`
+    document.body.style.overflowY = 'scroll'
+
+    return currentScrollY
+  }
+
+  const allowScroll = (prevScrollY: number) => {
+    document.body.style.position = ''
+    document.body.style.width = ''
+    document.body.style.top = ''
+    document.body.style.overflowY = ''
+
+    window.scrollTo(0, prevScrollY)
+  }
+
   useEffect(() => {
-    document.body.style.overflow = 'hidden'
+    const prevScrollY = preventScroll()
     return () => {
-      document.body.style.overflow = 'auto'
+      allowScroll(prevScrollY)
     }
   }, [])
 }
