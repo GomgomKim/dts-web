6bf9c978dbb11cbda4a6bd386a6631e996a9afd6
ebogyeong <bogyeong.lee@growdle.com>
2024-10-24 17:46:42 +0900

    feature: add enabled condition to useGetAuthProfile adapter

--- src/entities/UserProfile/model/adapter.ts
+++ src/entities/UserProfile/model/adapter.ts
@@ -5,12 +5,14 @@ import { getAuthProfile } from './api'
 import { AuthProfile, GetAuthProfileResData } from './types'
 
 export const useGetAuthProfile = () => {
+  const tokens = useAuthStore((state) => state.tokens)
   const isAuth = useAuthStore((state) => state.isAuth)
 
   return useQuery<GetAuthProfileResData, Error, AuthProfile>({
     queryKey: ['authProfile'],
     queryFn: () => getAuthProfile(),
     select: (data) => data.content,
-    enabled: isAuth === true
+    enabled: isAuth === true && tokens !== null,
+    throwOnError: true
   })
 }
