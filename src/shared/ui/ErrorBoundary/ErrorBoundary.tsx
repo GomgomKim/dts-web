fef1cfd702b23a9f5ce6a9d6972dfe06af5c3b56
ebogyeong <bogyeong.lee@growdle.com>
2024-10-25 10:46:21 +0900

    etc: remove comment

--- src/shared/ui/ErrorBoundary/ErrorBoundary.tsx
+++ src/shared/ui/ErrorBoundary/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
+'use client'
+
 import { Suspense } from 'react'
 
-// import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary'
 import { QueryErrorResetBoundary } from '@tanstack/react-query'
 
 import ReactErrorBoundary from './ReactErrorBoundary'
@@ -13,17 +14,6 @@ export function ErrorBoundary(props: ErrorBoundaryProps) {
   return (
     <QueryErrorResetBoundary>
       {({ reset }) => (
-        // <ReactErrorBoundary
-        //   onReset={reset}
-        //   fallbackRender={({ resetErrorBoundary }) => (
-        //     <div>
-        //       There was an error!
-        //       <Button onClick={() => resetErrorBoundary()}>Try again</Button>
-        //     </div>
-        //   )}
-        // >
-        //   <>{props.children}</>
-        // </ReactErrorBoundary>
         <ReactErrorBoundary
           FallbackComponent={props.FallbackComponent}
           onReset={reset} // 에러 발생 시 쿼리 재시도 설정
