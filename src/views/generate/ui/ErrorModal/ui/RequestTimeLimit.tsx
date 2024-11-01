18ff3971ca0de77b653a3aba3175853b6aabf001
ebogyeong <bogyeong.lee@growdle.com>
2024-11-01 16:21:45 +0900

    refactor: RequestTimeLimit with DefaultModal

---
+++ src/views/generate/ui/ErrorModal/ui/RequestTimeLimit.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+
+import { Button } from '@/shared/ui'
+import { DefaultModal } from '@/shared/ui/Modal/DefaultModal'
+import { ModalInfo } from '@/shared/ui/Modal/model/types'
+
+interface RequestTimeLimitProps extends Omit<ModalInfo, 'Component'> {}
+
+export const RequestTimeLimit = (props: RequestTimeLimitProps) => {
+  return (
+    <DefaultModal isClosable={true} onClose={props.onClose}>
+      <div>
+        <div className="mb-[2rem]">
+          <div className="text-[24px] mb-3">Take a Coffee Break!☕️</div>
+          <p className="text-neutral-7 text-[14px]">
+            Too many requests too fast! <br />
+            Give us a sec to catch up, and we’ll be ready soon.
+          </p>
+        </div>
+
+        <Button
+          className="py-[1rem] bg-white hover:bg-white"
+          stretch
+          onClick={props.onClose}
+        >
+          Try Again Soon
+        </Button>
+
+        <div className="mt-3 text-center">
+          <Link
+            href="https://tally.so/r/314QEg"
+            target="_blank"
+            className="text-[14px] underline underline-offset-4 p-3 inline-block text-center"
+          >
+            Feedback
+          </Link>
+        </div>
+      </div>
+    </DefaultModal>
+  )
+}
+RequestTimeLimit.displayName = 'RequestTimeLimit'
