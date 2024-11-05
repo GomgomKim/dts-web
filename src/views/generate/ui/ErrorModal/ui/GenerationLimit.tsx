b760682ff9148bc2c1e8933266a375d5721a2507
ebogyeong <bogyeong.lee@growdle.com>
2024-11-05 13:55:22 +0900

    feature: change button CTA in GenerationLimit error

--- src/views/generate/ui/ErrorModal/ui/GenerationLimit.tsx
+++ src/views/generate/ui/ErrorModal/ui/GenerationLimit.tsx
@@ -1,28 +1,16 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-
 import { Button } from '@/shared/ui'
 import { DefaultModal } from '@/shared/ui/Modal/DefaultModal'
 import { ModalComponentProps } from '@/shared/ui/Modal/model/types'
 
-import ArrowLeft from '/public/icons/arrow-thin.svg'
-
 interface GenerationLimitProps extends ModalComponentProps {}
 
 export const GenerationLimit = (props: GenerationLimitProps) => {
-  const router = useRouter()
-
   const { onCloseModal } = props
-
-  const handleCloseModal = () => {
-    onCloseModal()
-    router.push('/explore')
-  }
 
   return (
     <DefaultModal
-      closeable={{ isClosable: true, onClose: onCloseModal }}
       title="Generation Limit Reached for This Model"
       description={
         <>
@@ -35,14 +23,9 @@ export const GenerationLimit = (props: GenerationLimitProps) => {
           variant="destructive"
           className="py-[1rem]"
           stretch
-          onClick={handleCloseModal}
+          onClick={onCloseModal}
         >
-          <div className="flex items-center gap-[0.5rem]">
-            <ArrowLeft className="stroke-black -rotate-[135deg]" />
-            <span className="text-[0.75rem] font-semibold">
-              Go to Explore to Select Another Model
-            </span>
-          </div>
+          <span className="text-[0.75rem] font-semibold">OK, Got It</span>
         </Button>
       }
     />
