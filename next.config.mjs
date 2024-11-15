b6574be4ffe27b15d284a10a9ac50df1c586d56f
ebogyeong <bogyeong.lee@growdle.com>
2024-11-15 13:56:22 +0900

    etc: set sentry projectName depends on build env in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -32,47 +32,44 @@ const nextConfig = {
   }
 }
 
-const projectName =
-  process.env.NODE_ENV === 'development' ? 'dts-web-dev' : 'dts-web-prod'
+const buildEnv = process.env.NEXT_PUBLIC_ENVIRONMENT || 'development'
+
+let projectName
+switch (buildEnv) {
+  case 'development':
+    projectName = 'dts-web-dev'
+    break
+  case 'production':
+    projectName = 'dts-web-prod'
+    break
+  default:
+    projectName = 'dts-web-dev'
+}
 
 const sentryConfig = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
   org: 'growdle-k2',
   project: projectName,
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
   reactComponentAnnotation: {
     enabled: true
   },
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: '/monitoring',
-
-  // Hides source maps from generated client bundles
   hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true
 }
 
-export default withSentryConfig(nextConfig, sentryConfig)
+const configs = () => {
+  const isProduction = process.env.NODE_ENV === 'production'
+
+  if (isProduction) {
+    // production 환경에서만 Sentry 설정을 적용
+    return withSentryConfig(nextConfig, sentryConfig)
+  } else {
+    // 개발 환경에서는 Sentry 설정을 적용하지 않음
+    return nextConfig
+  }
+}
+
+export default configs
