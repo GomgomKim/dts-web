1ea69fd76e93380763582b46584ccfde43d2a734
이보경 <bogyeong.lee@growdle.com>
2024-11-19 15:39:26 +0900

    feature: update font style in Banner (#135)

--- src/views/explore/ui/Banner/Banner.tsx
+++ src/views/explore/ui/Banner/Banner.tsx
@@ -1,11 +1,11 @@
 export const Banner = () => {
   return (
     <div className="flex flex-col gap-6 text-center">
-      <div className="text-[32px] font-semibold">
+      <div className="text-[2rem] font-semibold">
         Create stunning virtual brand models effortlessly <br />
         with our advanced AI
       </div>
-      <div className="font-medium text-neutral-5">
+      <div className="text-[1.125rem] text-neutral-5">
         Enhance your brands presence with lifelike, customizable models
         generated in seconds
       </div>
