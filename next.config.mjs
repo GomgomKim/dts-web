8e5164c3b6cfbcc40bf53ecee4a55bead2a071ad
Kiyeun-Kim <kiyeun.kim@growdle.com>
2024-12-26 12:30:45 +0900

    Revert "Test2 (#23)" (#24)
    This reverts commit c01f887064c39094384e8a1c1d2f47c8a77a15c1.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+// import { withSentryConfig } from '@sentry/nextjs'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -7,18 +9,69 @@ const nextConfig = {
       test: /\.svg$/,
       use: ['@svgr/webpack']
     })
+
     return config
   },
   images: {
     remotePatterns: [
       {
+        protocol: process.env.NEXT_PUBLIC_API_PROTOCOL,
+        hostname: process.env.NEXT_PUBLIC_API_HOST_NAME,
+        port: process.env.NEXT_PUBLIC_API_PORT,
+        pathname: process.env.NEXT_PUBLIC_API_PATH_NAME
+      },
+      {
         protocol: 'https',
-        hostname: 'dev.api.dtsdevs.com',
-        port: '8080',
-        pathname: '/api/v2/**'
+        hostname: 'loremflickr.com'
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_API_GOOGLE_IMAGE_HOST_NAME
       }
     ]
   }
 }
+
+// const buildEnv = process.env.NEXT_PUBLIC_ENVIRONMENT || 'development'
+
+// let projectName
+// switch (buildEnv) {
+//   case 'development':
+//     projectName = 'dts-web-dev'
+//     break
+//   case 'production':
+//     projectName = 'dts-web-prod'
+//     break
+//   default:
+//     projectName = 'dts-web-dev'
+// }
+
+// const sentryConfig = {
+//   org: 'growdle-k2',
+//   project: projectName,
+//   silent: !process.env.CI,
+//   widenClientFileUpload: true,
+//   reactComponentAnnotation: {
+//     enabled: true
+//   },
+//   tunnelRoute: '/monitoring',
+//   hideSourceMaps: true,
+//   disableLogger: true,
+//   automaticVercelMonitors: true
+// }
+
+// const configs = () => {
+//   const isProduction = process.env.NODE_ENV === 'production'
+
+//   if (isProduction) {
+//     // production 환경에서만 Sentry 설정을 적용
+//     return withSentryConfig(nextConfig, sentryConfig)
+//   } else {
+//     // 개발 환경에서는 Sentry 설정을 적용하지 않음
+//     return nextConfig
+//   }
+// }
+
+// export default configs
 
 export default nextConfig
