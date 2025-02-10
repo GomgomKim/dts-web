86ae8f25534f1c32ab3b88bde2288e328c746783
ebogyeong <bogyeong.lee@growdle.com>
2025-02-10 17:05:32 +0900

    feat: pricing 페이지 plan, credit 탭 제작

--- src/views/pricing/model/constants.ts
+++ src/views/pricing/model/constants.ts
@@ -1,5 +1,9 @@
 export const UI_TEXT = {
+  PLAN: 'Plan',
+  CREDIT: 'Credit',
   FIND_YOUR_IDEAL_PLAN: 'Find Your Ideal Plan',
   PLAN_DESCRIPTION:
-    'Plans crafted to meet your brand’s needs, from startups to enterprises'
-}
+    'Plans crafted to meet your brand’s needs, from startups to enterprises',
+  GET_MORE_CREDITS: 'Get More Credits',
+  CREDIT_DESCRIPTION: 'Select the number of credits to add instantly'
+} as const
