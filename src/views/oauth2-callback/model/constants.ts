1ffabf83bd80805d39cd0411ae698886fb3e91b8
ebogyeong <bogyeong.lee@growdle.com>
2025-02-11 10:37:07 +0900

    feat: axios interceptor & auth api 엔드포인트 수정

--- src/views/oauth2-callback/model/constants.ts
+++ src/views/oauth2-callback/model/constants.ts
@@ -2,8 +2,8 @@
 export const PATH_PARAM_ERROR = 'PATH_PARAM_ERROR'
 export const UNKNOWN_ERROR = 'UNKNOWN_ERROR'
 
-export const URL_AUTH_SOCIAL = '/social'
-export const URL_AUTH_REGISTER = '/register'
+export const URL_AUTH_SOCIAL = '/auth/social'
+export const URL_AUTH_REGISTER = '/auth/register'
 
 export const SIGN_UP_AGREEMENT_VERSIONS = {
   confirmAdult: '1.0.0',
