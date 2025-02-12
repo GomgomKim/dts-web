ccbcc753d137e1063f4b5e1d27f0149d02540ac7
ebogyeong <bogyeong.lee@growdle.com>
2025-02-12 13:03:27 +0900

    feat: checkout 페이지 마크업

---
+++ app/checkout/layout.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from 'next/types'
+
+import { DefaultHeader } from '@/widgets/default-header'
+
+import { BackButton } from '@/entities/back-button'
+
+export const metadata: Metadata = {
+  title: 'Checkout'
+}
+
+export default function CheckoutLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <>
+      {/* TODO: header 수정 */}
+      <DefaultHeader />
+      <div className="h-screen pt-14">
+        <main className="px-10 pb-36 pt-5">
+          <BackButton className="mb-5" />
+          {children}
+        </main>
+      </div>
+    </>
+  )
+}
---
+++ src/views/checkout/model/constants.ts
@@ -0,0 +1,14 @@
+export const UI_TEXT = {
+  CHECKOUT: 'Checkout',
+  ORDER_SUMMARY: 'Order Summary',
+  CREDITS: 'Credits',
+  PLAN: 'Plan',
+  BILLING_CYCLE: 'Billing Cycle',
+  MONTHLY: 'Monthly',
+  PRICE: 'Price',
+  DISCOUNT: 'Discount',
+  SUBTOTAL: 'Subtotal',
+  AMOUNT_PAID: 'Amount Paid',
+  TAXES_INCLUDED: 'Taxes included',
+  PAY_NOW: 'Pay Now'
+} as const
