bafaf923d05ee3b67dcb96874baabe890d306c9d
ebogyeong <bogyeong.lee@growdle.com>
2025-02-12 13:17:38 +0900

    refactor: OrderSummary 컴포넌트화

---
+++ src/views/checkout/ui/order-summary/OrderSummary.tsx
@@ -0,0 +1,69 @@
+import { useSearchParams } from 'next/navigation'
+
+import { Button } from '@/shared/ui'
+
+import { UI_TEXT } from '../../model/constants'
+import { OrderLabeledDetail } from './ui'
+
+export const OrderSummary = () => {
+  const searchParams = useSearchParams()
+  const planId = searchParams.get('planId')
+  console.log(planId)
+
+  return (
+    <div className="m-auto w-[560px] divide-y rounded-[0.5rem] border border-neutral-2 p-10">
+      <h2 className="mb-8 text-[1.5rem] font-semibold">
+        {UI_TEXT.ORDER_SUMMARY}
+      </h2>
+      <div className="space-y-3 py-6">
+        <OrderLabeledDetail
+          label={UI_TEXT.PLAN}
+          detail="1 model plan (includes 20 credits)"
+        />
+        <OrderLabeledDetail
+          label={UI_TEXT.BILLING_CYCLE}
+          detail={
+            <>
+              <div className="mb-1 text-right text-[1.125rem] font-normal text-white">
+                {UI_TEXT.MONTHLY}
+              </div>
+              <div className="text-right text-[1rem] text-neutral-5">
+                2025. 02. 01 - 2025. 03. 01
+              </div>
+            </>
+          }
+        />
+      </div>
+      <div className="py-8">
+        <OrderLabeledDetail label={UI_TEXT.PRICE} detail={<>$100</>} />
+      </div>
+      <div className="divide-y pb-6 pt-8">
+        <div className="space-y-3 pb-6">
+          <OrderLabeledDetail label={UI_TEXT.DISCOUNT} detail={<>-</>} />
+          <OrderLabeledDetail label={UI_TEXT.SUBTOTAL} detail={<>$100</>} />
+        </div>
+        <div className="pt-6">
+          <OrderLabeledDetail
+            label={
+              <>
+                <span className="text-[1.125rem] font-bold">
+                  {UI_TEXT.AMOUNT_PAID}
+                </span>{' '}
+                <span className="text-[1.125rem] text-neutral-5">
+                  ({UI_TEXT.TAXES_INCLUDED})
+                </span>
+              </>
+            }
+            detail={<span className="text-[1.125rem] font-bold">$100</span>}
+          />
+        </div>
+      </div>
+      <div className="border-none">
+        {/* usd이면 페이팔 버튼 렌더링 */}
+        <Button stretch type="button" className="bg-white hover:bg-white">
+          {UI_TEXT.PAY_NOW}
+        </Button>
+      </div>
+    </div>
+  )
+}
