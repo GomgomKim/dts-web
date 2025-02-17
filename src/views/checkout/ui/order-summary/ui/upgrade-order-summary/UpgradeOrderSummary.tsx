923f6ee1cf54a59717da9f570c7d83a1e8543d7d
ebogyeong <bogyeong.lee@growdle.com>
2025-02-17 09:44:27 +0900

    feat: UpgradeOrderSummary 마크업

---
+++ src/views/checkout/ui/order-summary/ui/upgrade-order-summary/UpgradeOrderSummary.tsx
@@ -0,0 +1,100 @@
+import { useCurrencyStore } from '@/views/pricing/model/useCurrencyStore'
+import { PLAN_ITEMS } from '@/views/pricing/ui/plan-Items/model/constant'
+import { PLAN_NAME_TITLE_MAP } from '@/views/pricing/ui/plan-Items/model/types'
+
+import { OrderLabeledDetail } from '../OrderLabeledDetail'
+import { OrderSummaryContainer } from '../order-summary-container'
+import { PeriodOfUse } from '../period-of-use'
+import { UI_TEXT } from './model/constants'
+
+export const UpgradeOrderSummary = () => {
+  const currency = useCurrencyStore((state) => state.currency)
+  const currencySign = useCurrencyStore((state) => state.getCurrencySign())
+
+  const currentId = 2
+  const upgradeId = 4
+
+  const currentPlan = PLAN_ITEMS[currency].find((item) => item.id === currentId)
+  const upgradePlan = PLAN_ITEMS[currency].find((item) => item.id === upgradeId)
+
+  if (!currentPlan || !upgradePlan) return null
+
+  return (
+    <OrderSummaryContainer subtotal={0} discount={0}>
+      {/* section 1 */}
+      <div className="space-y-3 py-6">
+        <OrderLabeledDetail
+          label={UI_TEXT.CURRENT_PLAN}
+          detail={
+            <>
+              <div className="mb-1 text-right text-[1.125rem] font-normal text-white">
+                {PLAN_NAME_TITLE_MAP[currentPlan.name]} {UI_TEXT.PLAN}
+              </div>
+              <div className="text-right text-[1rem] text-neutral-5">
+                {currencySign}
+                {currentPlan.price} {UI_TEXT.PER_MONTH}
+              </div>
+            </>
+          }
+        />
+        <OrderLabeledDetail
+          label={UI_TEXT.NEW_PLAN}
+          detail={
+            <>
+              <div className="mb-1 text-right text-[1.125rem] font-normal text-white">
+                {PLAN_NAME_TITLE_MAP[upgradePlan.name]} {UI_TEXT.PLAN}
+              </div>
+              <div className="text-right text-[1rem] text-neutral-5">
+                {currencySign}
+                {upgradePlan.price} {UI_TEXT.PER_MONTH}
+              </div>
+            </>
+          }
+        />
+        <OrderLabeledDetail
+          label={UI_TEXT.BILLING_CYCLE}
+          detail={
+            <>
+              <div className="mb-1 text-right text-[1.125rem] font-normal text-white">
+                {UI_TEXT.MONTHLY}
+              </div>
+              <div className="text-right text-[1rem] text-neutral-5">
+                <PeriodOfUse />
+              </div>
+            </>
+          }
+        />
+      </div>
+
+      {/* section 2 */}
+      <div className="space-y-5 py-8">
+        <OrderLabeledDetail
+          label={UI_TEXT.PRORATED_CHARGE}
+          detail={
+            <>
+              <div className="mb-1 text-right text-[1.125rem] font-normal text-white">
+                {currencySign}하루가격
+              </div>
+              <div className="text-right text-[1rem] text-neutral-5">
+                ({UI_TEXT.FOR_N_DAYS_1} 남은일수 {UI_TEXT.FOR_N_DAYS_2})
+              </div>
+            </>
+          }
+        />
+        <OrderLabeledDetail
+          label={UI_TEXT.UNUSED_TIME_REFUND}
+          detail={
+            <>
+              <div className="mb-1 text-right text-[1.125rem] font-normal text-white">
+                -{currencySign}남은일수환불가격
+              </div>
+              <div className="text-right text-[1rem] text-neutral-5">
+                ({UI_TEXT.FOR_THE_REMAINING_DAYS})
+              </div>
+            </>
+          }
+        />
+      </div>
+    </OrderSummaryContainer>
+  )
+}
