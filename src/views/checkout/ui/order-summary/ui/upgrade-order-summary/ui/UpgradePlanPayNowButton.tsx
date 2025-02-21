8cdb72282b98ae650a81ed961bb6ac8c8c015100
ebogyeong <bogyeong.lee@growdle.com>
2025-02-21 14:42:27 +0900

    fix: import 수정 (next/router 2 next/navigation)

--- src/views/checkout/ui/order-summary/ui/upgrade-order-summary/ui/UpgradePlanPayNowButton.tsx
+++ src/views/checkout/ui/order-summary/ui/upgrade-order-summary/ui/UpgradePlanPayNowButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 import { throwIfNotAxiosError } from '@/shared/lib/utils/throwIfNotAxiosError'
 import { Button } from '@/shared/ui'
